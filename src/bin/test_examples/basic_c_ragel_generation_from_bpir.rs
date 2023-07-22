3f4bc9dfdb7b0fda97ea46d8e621d729b2b54516
DmitryMurashov <dmtr.murashov@gmail.com>
2023-07-22 18:10:00 +0300

    [bin:basic_c_ragel_generation_from_bpir] Ref | Comments

--- src/bin/test_examples/basic_c_ragel_generation_from_bpir.rs
+++ src/bin/test_examples/basic_c_ragel_generation_from_bpir.rs
@@ -1,7 +1,7 @@
 /// This exapmple provides a test run of Ragel-based C code generation.
 /// It uses raw BPIR which then is passed down the chain:
 ///
-/// BPIR -> [ Ragel generator ] -> Ragel/C code -> [ Ragel ] -> MAVLink C code parser
+/// BPIR -> [ Ragel generator ] -> Ragel/C code -> [ Ragel ] -> Parser
 
 use robusto;
 
