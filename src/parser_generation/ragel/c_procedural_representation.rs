c3e059aee4ef0d6ef924c8344d8385673b372ded
DmitryMurashov <dmtr.murashov@gmail.com>
2023-07-22 18:08:10 +0300

    [parser_generation:c_procedural_representation] Impl | Partial implementation of output routines

---
+++ src/parser_generation/ragel/c_procedural_representation.rs
@@ -0,0 +1,79 @@
+/// Generates C code from procedural representation
+
+use crate::bpir;
+use crate::parser_generation;
+use std;
+
+struct GenerationState {
+    /// Current indentation level
+    indent_level: usize,
+
+    indent_sequence: &'static str,
+    newline_sequence: &'static str,
+}
+
+impl GenerationState {
+    fn new() -> GenerationState {
+        GenerationState{
+            indent_level: 0,
+            indent_sequence: "\t",
+            newline_sequence: "\n",
+        }
+    }
+
+    fn write_indent<W: std::io::Write>(&self, buf_writer: &mut std::io::BufWriter<W>) {
+        use std::io::Write;
+
+        for i in 0..self.indent_level {
+            buf_writer.write(self.indent_sequence.as_bytes());
+        }
+    }
+
+    fn write_newline<W: std::io::Write>(&self, buf_writer: &mut std::io::BufWriter<W>) {
+        use std::io::Write;
+        buf_writer.write(self.newline_sequence.as_bytes());
+    }
+}
+
+/// C-specific Ragel AST
+pub struct CAst {
+    ast: parser_generation::ragel::procedural_representation::Ast
+}
+
+impl CAst {
+    fn new_from_protocol(protocol: &bpir::representation::Protocol) -> CAst {
+        CAst{
+            ast: parser_generation::ragel::procedural_representation::Ast::new_from_protocol(protocol),
+        }
+    }
+
+    fn generate_impl<W: std::io::Write>(generation_state: &mut GenerationState,
+        ast: &parser_generation::ragel::procedural_representation::Ast,
+        buf_writer: &mut std::io::BufWriter<W>
+    ) {
+        use parser_generation::ragel::procedural_representation::Ast;
+        use std::io::Write;
+
+        match ast {
+            Ast::Sequence{ref blocks} => {
+                for block in blocks {
+                    CAst::generate_impl(generation_state, block, buf_writer);
+                }
+            },
+            Ast::MachineHeader{machine_name} => {
+                buf_writer.write_fmt(format_args!(
+                    "{}\n
+                    \tmachine {:?}\n;
+                    \twrite data;\n
+                    {}", "%%{", machine_name, "%%}"
+                ));
+            }
+            _ => {}
+        }
+    }
+}
+
+impl parser_generation::Generate for CAst {
+    fn generate<W: std::io::Write>(&self, buf_writer: &mut std::io::BufWriter<W>) {
+    }
+}
