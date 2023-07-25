43cb3e649455105573e314956b28f3bf2083ef87
DmitryMurashov <dmtr.murashov@gmail.com>
2023-07-25 21:20:52 +0300

    [bin:basic_c_ragel_generation_from_bpir] Impl |

--- src/bin/test_examples/basic_c_ragel_generation_from_bpir.rs
+++ src/bin/test_examples/basic_c_ragel_generation_from_bpir.rs
@@ -1,9 +1,45 @@
+use std::str::FromStr;
+
 /// This exapmple provides a test run of Ragel-based C code generation.
 /// It uses raw BPIR which then is passed down the chain:
 ///
 /// BPIR -> [ Ragel generator ] -> Ragel/C code -> [ Ragel ] -> Parser
 
 use robusto;
+use std;
+
+const OUTPUT_FILE_NAME: &'static str = "output.c.rl";
+
+fn make_message_bpir() -> robusto::bpir::representation::Message {
+	let mut message = robusto::bpir::representation::Message{
+		name: std::string::String::from("TestMessage"),
+		fields: std::vec::Vec::<robusto::bpir::representation::Field>::new(),
+		attributes: std::vec::Vec::<robusto::bpir::representation::MessageAttribute>::new(),
+	};
+
+	message.fields.push(robusto::bpir::representation::Field{
+		name: std::string::String::from("preamble"),
+		attributes: vec![
+			robusto::bpir::representation::FieldAttribute::ExpectConstSequence(vec![0xfe]),
+		]
+	});
+	message.fields.push(robusto::bpir::representation::Field{
+		name: std::string::String::from("payload"),
+		attributes: vec![
+			robusto::bpir::representation::FieldAttribute::ExpectLength(3),
+		]
+	});
+
+	message
+}
 
 fn main() {
+	use robusto::parser_generation::Generate;
+	use std::io::Write;
+
+	let protocol = robusto::bpir::representation::Protocol{messages: vec![make_message_bpir()]};
+	let file = std::fs::File::create(OUTPUT_FILE_NAME).unwrap();
+	let mut buf_writer = std::io::BufWriter::new(file);
+	let c_ast = robusto::parser_generation::ragel::c_procedural_representation::CAst::new_from_protocol(&protocol);
+	c_ast.generate(&mut buf_writer);
 }
