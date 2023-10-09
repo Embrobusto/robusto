c03eef9e1e77887c3aad411fcc84ac7bc8d42de4
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-09 21:28:46 +0300

    [test_examples:basic_c_ragel_generation_from_bpir] Ref | Indentation

--- src/bin/test_examples/basic_c_ragel_generation_from_bpir.rs
+++ src/bin/test_examples/basic_c_ragel_generation_from_bpir.rs
@@ -12,42 +12,42 @@ use env_logger;
 const OUTPUT_FILE_NAME: &'static str = "output.c.rl";
 
 fn make_message_bpir() -> robusto::bpir::representation::Message {
-	let mut message = robusto::bpir::representation::Message{
-		name: std::string::String::from("TestMessage"),
-		fields: std::vec::Vec::<robusto::bpir::representation::Field>::new(),
-		attributes: std::vec::Vec::<robusto::bpir::representation::MessageAttribute>::new(),
-	};
+    let mut message = robusto::bpir::representation::Message{
+        name: std::string::String::from("TestMessage"),
+        fields: std::vec::Vec::<robusto::bpir::representation::Field>::new(),
+        attributes: std::vec::Vec::<robusto::bpir::representation::MessageAttribute>::new(),
+    };
 
-	message.fields.push(robusto::bpir::representation::Field{
-		name: std::string::String::from("preamble"),
-		attributes: vec![
-			robusto::bpir::representation::FieldAttribute::ConstSequence(vec![0xfe]),
-		]
-	});
-	message.fields.push(robusto::bpir::representation::Field{
-		name: std::string::String::from("payload"),
-		attributes: vec![
-			robusto::bpir::representation::FieldAttribute::Length(3),
-		]
-	});
+    message.fields.push(robusto::bpir::representation::Field{
+        name: std::string::String::from("preamble"),
+        attributes: vec![
+            robusto::bpir::representation::FieldAttribute::ConstSequence(vec![0xfe]),
+        ]
+    });
+    message.fields.push(robusto::bpir::representation::Field{
+        name: std::string::String::from("payload"),
+        attributes: vec![
+            robusto::bpir::representation::FieldAttribute::Length(3),
+        ]
+    });
 
-	message
+    message
 }
 
 fn main() {
-	use robusto::parser_generation::Generate;
-	use std::io::Write;
+    use robusto::parser_generation::Generate;
+    use std::io::Write;
 
-	// Initialize logging
-	env_logger::init();
+    // Initialize logging
+    env_logger::init();
 
-	// Create a simple BPIR
-	let protocol = robusto::bpir::representation::Protocol{messages: vec![make_message_bpir()]};
+    // Create a simple BPIR
+    let protocol = robusto::bpir::representation::Protocol{messages: vec![make_message_bpir()]};
 
-	// Run Ragel code generation
-	let file = std::fs::File::create(OUTPUT_FILE_NAME).unwrap();
-	let mut buf_writer = std::io::BufWriter::new(file);
-	let ast = robusto::parser_generation::ragel::common::AstNode::from_protocol(&protocol);
-	let c_generator = robusto::parser_generation::ragel::c::Generator::from_ragel_ast(&ast);
-	c_generator.write(&mut buf_writer);
+    // Run Ragel code generation
+    let file = std::fs::File::create(OUTPUT_FILE_NAME).unwrap();
+    let mut buf_writer = std::io::BufWriter::new(file);
+    let ast = robusto::parser_generation::ragel::common::AstNode::from_protocol(&protocol);
+    let c_generator = robusto::parser_generation::ragel::c::Generator::from_ragel_ast(&ast);
+    c_generator.write(&mut buf_writer);
 }
