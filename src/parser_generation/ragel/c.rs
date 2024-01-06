6f1a652eadb22e58733cc2c895185ad37b9a9f45
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-06 19:56:20 +0300

    [ragel:c] Impl | C parser state generation

--- src/parser_generation/ragel/c.rs
+++ src/parser_generation/ragel/c.rs
@@ -2,12 +2,12 @@
 use crate::bpir;
 use crate::bpir::representation::RegexFieldType;
 use crate::parser_generation;
+use crate::utility;
 use log;
 use std;
 use std::fmt::write;
 use std::io::Write;
 use std::str::FromStr;
-use crate::utility;
 
 const NEWLINE: &'static str = "\n";
 
@@ -39,8 +39,15 @@ impl Generator<'_> {
         generation_state: &mut GenerationState,
     ) {
         match ast_node.ast_node_type {
-            parser_generation::ragel::common::Ast::MachineHeader(ref node) => {
-                self.generate_machine_name(ast_node, buf_writer, &node.machine_name, generation_state)
+            parser_generation::ragel::common::Ast::MachineHeader(ref node) => self
+                .generate_machine_header(
+                    ast_node,
+                    buf_writer,
+                    &node.machine_name,
+                    generation_state,
+                ),
+            parser_generation::ragel::common::Ast::ParserState(ref node) => {
+                self.generate_parser_state(ast_node, buf_writer, &node, generation_state)
             }
             parser_generation::ragel::common::Ast::None => {
                 for ast_node_child in &ast_node.children {
@@ -48,12 +55,7 @@ impl Generator<'_> {
                 }
             }
             parser_generation::ragel::common::Ast::ParsingFunction(ref node) => {
-                self.generate_parsing_function(
-                    ast_node,
-                    buf_writer,
-                    &node,
-                    generation_state,
-                );
+                self.generate_parsing_function(ast_node, buf_writer, &node, generation_state);
             }
             _ => {
                 log::error!(
@@ -65,20 +67,52 @@ impl Generator<'_> {
         }
     }
 
-    fn generate_machine_name<W: std::io::Write>(
+    fn generate_machine_header<W: std::io::Write>(
         &self,
         ast_node: &parser_generation::ragel::common::AstNode,
         buf_writer: &mut std::io::BufWriter<W>,
         machine_name: &std::string::String,
         generation_state: &mut GenerationState,
     ) {
-        utility::string::write_with_indent_or_panic(buf_writer, generation_state.indent, format!(
-"%%{{
+        utility::string::write_with_indent_or_panic(
+            buf_writer,
+            generation_state.indent,
+            format!(
+                "%%{{
     machine {machine_name};
     write data;
 %%}}
 "
-        ).as_bytes());
+            )
+            .as_bytes(),
+        );
+    }
+
+    fn generate_parser_state<W: std::io::Write>(
+        &self,
+        ast_node: &parser_generation::ragel::common::AstNode,
+        buf_writer: &mut std::io::BufWriter<W>,
+        node: &parser_generation::ragel::common::ParserStateAstNode,
+        generation_state: &mut GenerationState,
+    ) {
+        utility::string::write_with_indent_or_panic(
+            buf_writer,
+            generation_state.indent,
+            format!(
+"
+struct {0}ParserState {{
+    int isInitialized;
+    int isError;
+}};
+
+static struct {0}ParserState s{0}ParserState {{
+    .isInitialized = 0,
+    .isError = 0,
+}};
+
+",
+            node.name).as_bytes(),
+        );
     }
 
     fn generate_parsing_function<W: std::io::Write>(
@@ -92,12 +126,16 @@ impl Generator<'_> {
         utility::string::write_line_with_indent_or_panic(
             buf_writer,
             generation_state.indent,
-            format!("void parse{0}(const char *aInputBuffer, int aInputBufferLength, struct {1} *a{2})", node.message_name, node.message_name, node.message_name,).as_bytes()
+            format!(
+                "void parse{0}(const char *aInputBuffer, int aInputBufferLength, struct {1} *a{2})",
+                node.message_name, node.message_name, node.message_name,
+            )
+            .as_bytes(),
         );
         utility::string::write_line_with_indent_or_panic(
             buf_writer,
             generation_state.indent,
-            "{".as_bytes()
+            "{".as_bytes(),
         );
         generation_state.indent += 1;
         utility::string::write_line_with_indent_or_panic(
@@ -113,14 +151,22 @@ impl Generator<'_> {
         utility::string::write_line_with_indent_or_panic(
             buf_writer,
             generation_state.indent,
-            format!( "int cs;  // Current state -- Ragel-specific variable for C code generation").as_bytes(),
+            format!("int cs;  // Current state -- Ragel-specific variable for C code generation")
+                .as_bytes(),
         );
 
         // Iterate through children
         for child_node in &ast_node.children {
             match child_node.ast_node_type {
-                parser_generation::ragel::common::Ast::RawStringSequence(ref node) => {self.generate_raw_string_sequence_parser(child_node, buf_writer, node, generation_state); },
-                _ => {}  // TODO: MUST NOT get here
+                parser_generation::ragel::common::Ast::RawStringSequence(ref node) => {
+                    self.generate_raw_string_sequence_parser(
+                        child_node,
+                        buf_writer,
+                        node,
+                        generation_state,
+                    );
+                }
+                _ => {} // TODO: MUST NOT get here
             }
         }
     }
@@ -133,7 +179,6 @@ impl Generator<'_> {
         generation_state: &mut GenerationState,
     ) {
     }
-
 }
 
 impl parser_generation::Write for Generator<'_> {
