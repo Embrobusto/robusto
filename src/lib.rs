91255ece1e9319d4acb7ce1b55d7bf8ebf172646
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-07 15:58:41 +0300

    [src:lib] Ref | Module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! Embedded binary protocol serializer / deserializer generation library.
+//!
+//! Robusto is a Rust-based framework for generating binary protocol serializers
+//! and deserializers. Among its functions, the primary one is to provide a
+//! convenient frontent to [Ragel](https://github.com/adrian-thurston/ragel)
+//! which generates an intermediate languange-dependent code which then gets
+//! processed by Ragel for generating a resulting parser.
+//!
+//! All serial embedded binary protocols share a great deal of similarities.
+//! Robusto exploits this feature to off-load this responsibility from embedded
+//! programmers. Robusto's mission is to provide one-stop solution for embedded
+//! binary protocol implementation, to cut-off the risks and time expenses
+//! entailed by hand-writing binary protocol-parsing / marshalling boilerplate
+//! code.
+
 pub mod parser_generation;
 pub mod bpir;
 mod utility;
