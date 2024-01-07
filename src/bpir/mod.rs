7cf1b6949499d4c74327218bc6d084498465d2b3
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-07 16:19:08 +0300

    [bpir:mod] Ref |

--- src/bpir/mod.rs
+++ src/bpir/mod.rs
@@ -1,8 +1,8 @@
 //! BPIR stands for "Binary Protocol Intermediate Representation". It is the
 //! core of "robusto" library, the intermediate representation for binary
-//! protocols which is, in a long shot, an to cover each and every serial
-//! embedded binary protocol: CRC checksums, preambles and parser
-//! synchronization, conditional interpretation, etc.
+//! protocols which, in a long shot, covers each and every serial embedded
+//! binary protocol: CRC checksums, preambles and parser synchronization,
+//! conditional interpretation, etc.
 
 pub mod representation;
 pub mod validation;
